50378ead11f44035c6b772c9a4634fe7d212b999
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-12-04 18:16:21 +0100

    add: stl cat added

--- test_files/script.js
+++ test_files/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { STLLoader } from 'three/addons/loaders/STLLoader.js';
 
 const iw = window.innerWidth;
 const ih = window.innerHeight;
@@ -8,8 +9,9 @@ const scene = new THREE.Scene();
 
 // Créer une caméra
 const camera = new THREE.PerspectiveCamera(70, iw/ih, 0.1, 1000);
-camera.position.set(0, 0, 2);
+camera.position.set(0, 0, 10);
 
+/*
 // Créer et ajouter un mesh à la scène
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const texture = new THREE.TextureLoader().load('/textures/wood_texture.jpg')
@@ -17,14 +19,32 @@ const texture = new THREE.TextureLoader().load('/textures/wood_texture.jpg')
 const material = new THREE.MeshPhongMaterial({map: texture});
 const mesh = new THREE.Mesh(geometry, material);
 
-// Ajoute une lumière
-const light = new THREE.PointLight(0xeeeeee);
-light.position.set(0, 0, 2);
-
 scene.add(mesh);
+*/
+
+// importer un fichier STL
+const loader = new STLLoader();
+loader.load('stl_file/cat.stl', (geometry) => {
+    //const text = new THREE.TextureLoader().load('/stl_file/cat_text.jpg');
+    const material = new THREE.MeshStandardMaterial({color: 0xffffffff});
+    const cat_mesh = new THREE.Mesh(geometry, material);
+    console.log("cat found")
+
+    cat_mesh.scale.set(0.1, 0.1, 0.1);
+    cat_mesh.position.set(0, 0, 0);
+
+    scene.add(cat_mesh)
+})
+
+// Ajoute une lumière
+const light = new THREE.PointLight(0xeeeeee, 1000);
+light.position.set(0, 0, 100);
 scene.add(light);
 
-// Créer le moteur de rendu sur une balise
+const ambientLight = new THREE.AmbientLight(0x404040, 2);
+scene.add(ambientLight);
+
+// Créer le moteur de rendu dans une balise contenant un ID
 const canvas = document.getElementById('canva');
 const renderer = new THREE.WebGLRenderer({canvas});
 
@@ -35,8 +55,10 @@ loop();
 // Anime le cube
 function loop() {
     requestAnimationFrame(loop)
+    /*
     mesh.rotation.y += 0.01
     mesh.rotation.x += 0.005
-    // Lancer un rendu avec la scène et la caméra
-    renderer.render(scene, camera);
+    */
+   // Lancer un rendu avec la scène et la caméra
+   renderer.render(scene, camera);
 }
