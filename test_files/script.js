2d7734b1d6d314841a83f95a4abaf59e0ed3eb6f
GuillaumeLerayGirardeau <guiraydeau@gmail.com>
2025-12-04 14:27:54 +0100

    add: now handle textures

--- test_files/script.js
+++ test_files/script.js
@@ -12,9 +12,17 @@ camera.position.set(0, 0, 2);
 
 // Créer et ajouter un mesh à la scène
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({color: '#424242'});
+const texture = new THREE.TextureLoader().load('/textures/wood_texture.jpg')
+// le Phong Material est un shadder standard qui simule une réaction à une source de lumière
+const material = new THREE.MeshPhongMaterial({map: texture});
 const mesh = new THREE.Mesh(geometry, material);
+
+// Ajoute une lumière
+const light = new THREE.PointLight(0xeeeeee);
+light.position.set(0, 0, 2);
+
 scene.add(mesh);
+scene.add(light);
 
 // Créer le moteur de rendu sur une balise
 const canvas = document.getElementById('canva');
@@ -22,12 +30,13 @@ const renderer = new THREE.WebGLRenderer({canvas});
 
 renderer.setSize(iw, ih);
 
-loop()
+loop();
 
 // Anime le cube
 function loop() {
     requestAnimationFrame(loop)
     mesh.rotation.y += 0.01
+    mesh.rotation.x += 0.005
     // Lancer un rendu avec la scène et la caméra
     renderer.render(scene, camera);
 }
